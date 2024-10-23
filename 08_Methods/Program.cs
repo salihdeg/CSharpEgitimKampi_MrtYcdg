e867b63712c965ce171d511fcfec27ab7051775b
Salih Değirmenci <salihdegirmenci99@gmail.com>
2024-10-23 18:20:03 +0300

    Day 8

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Metotlar
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("1. Salih Değirmenci");
+            //    Console.WriteLine("2. Kadir Ali Demiröz");
+            //    Console.WriteLine("3. Ecem Yıldız");
+            //    Console.WriteLine("4. Buse Omay");
+            //}
+
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+
+            //void Sum()
+            //{
+            //    int x = 1, y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+
+            //Sum();
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen String Parametreli Metotlar
+
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+
+            //WriteMethod("Buse Kara");
+
+            //void CustomerCard(string name, string surname)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + surname);
+            //}
+
+            //CustomerCard("Mehmet", "Şimşek");
+            //CustomerCard("Kadir Ali", "Demiröz");
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
+
+            //void Sum(int number1, int number2, int number3)
+            //{
+
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+
+            //}
+
+            //Sum(4, 5, 6);
+            #endregion
+
+            #region Geriye Değer Döndüren Metotlar
+
+            //string CustomerName()
+            //{
+            //    return "Kadir Ali";
+            //}
+
+            //CustomerName();
+
+            //string StudentCard() {
+
+            //    string name = "Salih";
+            //    string surname = "Değirmenci";
+
+            //    return name + " " + surname;
+            //}
+
+            //Console.WriteLine(StudentCard());
+
+            #endregion
+
+            #region Geriye Değer Döndüren String Parametreli Metotlar
+
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+
+            //    string cardInfo = "Ülke: "+ countryName + " - Başkent: "
+            //        + capital + " - Bayrak Rengi: " + flagColor;
+
+            //    return cardInfo;
+            //}
+
+            //string x, y, z;
+
+            //Console.Write("Ülke Adını Giriniz: ");
+            //x = Console.ReadLine();
+
+            //Console.Write("Başkenti Giriniz: ");
+            //y = Console.ReadLine();
+
+            //Console.Write("Bayrak Rengini Giriniz: ");
+            //z = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x,y,z));
+            //Console.WriteLine(CountryCard("Türkiye","Ankara","Kırmızı - Beyaz"));
+
+            #endregion
+
+            #region Geriye Değer Döndüren Int Parametreli Metotlar
+
+            ////int Sum(int number1, int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+
+            //Console.WriteLine(Sum(80, 98));
+            //Console.WriteLine(Sum(60, 25));
+            //Console.WriteLine(Sum(20, 50));
+            //Console.WriteLine(Sum(10, 90));
+
+            #endregion
+
+            #region Örnek Uygulama
+
+            //string Examresult(string student, int exam1, int exam2, int exam3)
+            //{
+            //    int result = (exam1 + exam2 + exam3) / 3;
+
+            //    if (result >= 50)
+            //    {
+            //        return student + " isimli öğrenci sınavı geçti. " + "Ortalama: " + result;
+            //    }
+            //    else
+            //    {
+            //        return student + " isimli öğrenci sınavı geçemedi. Ortalama: " + result;
+            //    }
+            //}
+
+            //Console.WriteLine(Examresult("Salih", 78, 41, 85));
+            //Console.WriteLine(Examresult("Kadir Ali", 25, 41, 32));
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
